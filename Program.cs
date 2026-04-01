9424d1bb3adf9e8b7b068301d2d4552bbae29dd5
Jessim <benrejebjessim@gmail.com>
2026-04-01 16:40:08 +0200

    Ajout détection manette et test avec l'input "A"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Windows.Gaming.Input;
+
+Gamepad gamepad = null;
+bool connected = true;
+
+while (gamepad == null)
+{
+    Console.WriteLine("BRANCHEZ MANETTE VITE VITE VITE VITE");
+
+    for (int i = 0; i < RawGameController.RawGameControllers.Count; i++)
+    {
+        RawGameController rawGameController = RawGameController.RawGameControllers[i];
+        gamepad = Gamepad.FromGameController(rawGameController);
+        if (gamepad != null) 
+        {
+            break;
+        }
+    }
+}
+
+while(connected)
+{
+    GamepadReading reading = gamepad.GetCurrentReading();
+
+    if (GamepadButtons.A == (reading.Buttons & GamepadButtons.A))
+    {
+        Console.WriteLine("A pressed !");
+    } else {
+        Console.WriteLine("NOTHINGU");
+    }
+}
+
+Console.WriteLine("Hello World !");
